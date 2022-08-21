83e9ef6bcc5b318e09f81b5d03c8facd1c55508b
konstantin <kostich_smnv@mail.ru>
2022-08-21 13:56:09 +0300

    fix mistakes

--- Edit-popup.js
+++ Edit-popup.js
@@ -8,10 +8,6 @@ const profileNameEdit = document.querySelector('.profile__popup-name');
 const profileStatus = document.querySelector('.profile__popup-second-name');
 const editProfileButton = document.querySelector('.save__profile-popup');
 const profileEditSecondName = document.querySelector('.profile__info-subtitle');
-const disabledLikes = document.querySelectorAll('.photo__grade');
-
-
-
 
 
 
@@ -44,11 +40,6 @@ function editSecondName() {
 
 
 
-for ( const disabledLike of disabledLikes) {
-    disabledLike.addEventListener('click', () => {
-        disabledLike.classList.toggle('photo__grade_active')
-    });
-};
 
 
 
@@ -63,6 +54,78 @@ editProfileButton.addEventListener('click', () => {
     editSecondName();
     closeEdit();
 });
+
+
+
+
+const addPhotoButton = document.querySelector('.add__photo');
+const addPhotoPopup = document.querySelector('.add__photo-popup');
+const closePhotoPopup = document.querySelector('.close__add_photo-popup');
+
+
+
+
+closePhotoPopup.addEventListener('click', () => {
+    saveAddition()
+});
+
+addPhotoButton.addEventListener('click', () => {
+    openAddition()
+});
+
+
+function openAddition() {
+    const addPhotoPopupInfo = document.querySelector('.add__photo-popup-info');
+    addPhotoPopup.classList.add('add__photo-popup_active');
+    addPhotoPopupInfo.classList.add('add__photo-popup-info_active')
+};
+
+
+const gallery = document.querySelector('.gallery');
+const saveAddButton = document.querySelector('.save__add__photo-popup');
+const disabledLikes = document.querySelectorAll('.photo__grade');
+
+
+
+saveAddButton.addEventListener('click', () => {
+    let addUrl = document.querySelector('.add__photo-popup-second-name');
+    let addNamepost = document.querySelector('.add__photo-popup-name');
+    gallery.insertAdjacentHTML('afterBegin', `
+    <div class="gallery__photo">
+        <img src="${getUrl(addUrl)}" alt="Karachevsk" class="photo">
+        <div class="photo__info">
+            <h2 class="photo__info-title">${getNamePost(addNamepost)}</h2>
+            <div class="photo__grade"></div>
+        </div>
+    </div>`);
+    saveAddition()
+    addUrl.value =''
+})
+
+function saveAddition() {
+    addPhotoPopup.classList.remove('add__photo-popup_active');
+}
+
+
+function getUrl(element) {
+    let newUrl = element.value;
+    return newUrl;
+}
+
+function getNamePost(element) {
+    let newNamePost = element.value;
+    return newNamePost;
+}
+
+
+
+for ( const disabledLike of disabledLikes) {
+    disabledLike.addEventListener('click', () => {
+        disabledLike.classList.toggle('photo__grade_active')
+    });
+};
+
+
 
 
 
